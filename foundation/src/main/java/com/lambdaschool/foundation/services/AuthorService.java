c6fd7bf603837c745cd5a6ab836b6b49275852da
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-06-19 11:59:02 -0700

    set up the author service

--- foundation/src/main/java/com/lambdaschool/foundation/services/AuthorService.java
+++ foundation/src/main/java/com/lambdaschool/foundation/services/AuthorService.java
@@ -2,7 +2,10 @@ package com.lambdaschool.foundation.services;
 
 import com.lambdaschool.foundation.models.Author;
 
+import java.util.List;
+
 public interface AuthorService
 {
+   List<Author> findAllAuthors();
    Author save(Author newAuthor);
 }
--- foundation/src/main/java/com/lambdaschool/foundation/services/AuthorServiceImpl.java
+++ foundation/src/main/java/com/lambdaschool/foundation/services/AuthorServiceImpl.java
@@ -6,12 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Transactional
 @Service(value = "authorService")
 public class AuthorServiceImpl implements AuthorService
 {
    @Autowired
    private AuthorRepository authrepos;
+
+   @Override
+   public List<Author> findAllAuthors()
+   {
+      List<Author> list = new ArrayList<>();
+      /*
+       * findAll returns an iterator set.
+       * iterate over the iterator set and add each element to an array list.
+       */
+      authrepos.findAll()
+              .iterator()
+              .forEachRemaining(list::add);
+      return list;
+   }
 
    @Transactional
    @Override
